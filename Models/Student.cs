870c9a661c2e3c1724ab67765b5ee6f95a0973f0
Manoj Khatri <manoj.khatri@vesuvio.io>
2022-05-28 23:48:53 +0545

    database connected and context for department and student created

---
+++ Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Required")]
+        public int Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public int Fname { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.EmailAddress)]
+        public int Email { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public int Mobile { get; set; }
+
+        public int Description { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name="Department")]
+        public int DepID { get; set; }
+
+
+        [NotMapped]
+        public string Department { get; set; }
+
+    }
+}
